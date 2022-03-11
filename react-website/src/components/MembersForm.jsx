47757de17c7233d033a9311e6b9c1419e1c3e3f6
Rafael Franco <francosprivato@gmail.com>
2022-03-11 15:43:17 -0300

    template for members form

--- react-website/src/components/MembersForm.jsx
+++ react-website/src/components/MembersForm.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SimpleInput from "../components/shared/SimpleInput";
+import "../styles/MembersForm.css";
 
 const MembersForm = () => {
-  return <div></div>;
+  return (
+    <form className="Members-form">
+      <div className="Form-inputs">
+        <SimpleInput placeholder={"First name"} name={"name"} />
+        <SimpleInput placeholder={"Last name"} name={"lastname"} />
+        <SimpleInput placeholder={"Address"} name={"address"} />
+        <SimpleInput placeholder={"SSN"} name={"ssn"} />
+        <span>Error Message</span>
+      </div>
+      <div className="Form-buttons">
+        <button>Reset</button>
+        <button>Save</button>
+      </div>
+    </form>
+  );
 };
 
 export default MembersForm;
