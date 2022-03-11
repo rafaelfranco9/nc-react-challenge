1511c28bd04376fe2e35d943ed5a09969a68f705
Rafael Franco <francosprivato@gmail.com>
2022-03-11 20:15:09 -0300

    Creating members context

---
+++ react-website/src/contexts/MembersContext.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+import useAuth from "../hooks/useAuthentication";
+
+export const MembersContext = React.createContext({});
+
+export const MembersContextProvider = ({ children }) => {
+  const {
+    token: { token },
+  } = useAuth("sarah", "connor");
+  const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getAllMembersFromApi = async () => {
+    setLoading(true);
+    try {
+      const response = await axios("http://localhost:8081/api/members", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        setMembers(response.data);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+    setLoading(false);
+  };
+
+  const saveNewMember = async (newMember) => {
+    setLoading(true);
+    try {
+      const response = await axios("http://localhost:8081/api/members", {
+        method: "POST",
+        data: newMember,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        setMembers([...members, response.data]);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+    setLoading(false);
+  };
+
+  const values = {
+    saveNewMember,
+    getAllMembersFromApi,
+    members,
+    loading,
+  };
+
+  return (
+    <MembersContext.Provider value={values}>{children}</MembersContext.Provider>
+  );
+};
