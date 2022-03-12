e5c31b08ba99b8859d4c9a2f1bb36fb09db9eb4b
Rafael Franco <francosprivato@gmail.com>
2022-03-12 00:42:16 -0300

    implementing inactivity refresh

--- react-website/src/containers/Members.jsx
+++ react-website/src/containers/Members.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MembersPanel from "../components/MembersPanel";
 import { MembersContextProvider } from "../contexts/MembersContext";
 import "../styles/Members.css";
 
 const Members = () => {
+  const lastActivity = useRef(new Date().getTime());
+
+  const updateTime = () => {
+    lastActivity.current = new Date().getTime();
+  };
+
+  useEffect(() => {
+    window.addEventListener("keypress", updateTime);
+    refreshOnInactivity();
+    return () => {
+      window.removeEventListener("keypress", updateTime);
+    };
+  }, []);
+
+  const refreshOnInactivity = () => {
+    if (new Date().getTime() - lastActivity.current >= 120000) {
+      window.location.reload();
+    } else {
+      setTimeout(refreshOnInactivity, 30000);
+    }
+  };
+
   return (
     <MembersContextProvider>
       <div className="Members-view">
